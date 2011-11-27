fea1b33dedb3f786ce1794222596dd3b7a38483d
Claas Abert <=>
2011-11-27 15:54:39 +0100

    clear

--- install.rb
+++
@@ -1,8 +0,0 @@
-require 'fileutils'
-
-# setup the font path
-font_path = File.join(RAILS_ROOT, 'fonts')
-FileUtils.mkpath(font_path)
-
-# show the README file
-puts IO.read(File.join(File.dirname(__FILE__), 'README'))
