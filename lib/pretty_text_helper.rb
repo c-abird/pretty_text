1b8a47aaafae9d66c6bc0a4a6b285dd7c40e1541
Claas Abert <=>
2011-11-27 15:56:06 +0100

    Initial commit

---
+++ lib/pretty_text_helper.rb
@@ -0,0 +1,54 @@
+module ApplicationHelper
+  # This helper creates a <tt>PrettyText</tt> object and shows the generated text as
+  # background image of a HTML-tag.
+  # You can set the style of the rendered text by a <tt>PrettyTextStyle</tt> object
+  # or by a hash:
+  #
+  #   <% @style = Cabird::PrettyTextStyle.new(:color => '#666666', :size => 30) %>
+  #   <%= pretty_text("the text", @style, 'h1')
+  #
+  # or
+  #
+  #   <%= pretty_text("the text", {:color => '#666666', :size => 30}, 'h1')
+  #
+  # which will result in the same output:
+  #
+  #   <h1 style="... background: url('...'); ...">the text</h1>
+  #
+  # Using the <tt>PrettyTextStyle</tt> class gives you the opportunity to use a kind of
+  # style inheritance:
+  #
+  #   <% @style = Cabird::PrettyTextStyle.new(:color => '#666666', :size => 30) %>
+  #   <%= pretty_text("a headline", @style, 'h1')
+  #   <%= pretty_text("normal text", @style.alter(:size => 12), 'p')
+  #
+  # Use the html_options to append HTML options to the tag:
+  #   <%= pretty_text("the text", @style, 'h1', {:class => "myclass"})
+  def pretty_text(str, style = nil, tag="span", html_options = {})
+    text = PrettyText::Text.create(str, style);
+
+    html_options[:class] = [html_options[:class]].push("pretty_text").compact.join(" ")
+    #convert_options_to_javascript!(html_options)
+    tag_options = tag_options(html_options)
+
+    style  = "width:#{text.width}px;padding-top:#{text.height}px;"
+    style << "background-image: url('#{image_path(text.path)}');"
+
+    ret =  "<#{tag} style=\"#{style}\"#{tag_options}>"
+    ret << h(text.content)
+    ret << "</#{tag}>"
+
+    raw(ret)
+  end
+
+  # This method returns the public path to the rendered text. The parameters are the same
+  # as the two first parameters of the pretty_text method.
+  def pretty_text_path(str, style = nil)
+    text = PrettyText::Text.create(str, style);
+    return text.path
+  end
+
+  def pretty_text_includes
+    stylesheet_link_tag('pretty_text')
+  end
+end
---
+++ lib/pretty_text_style.rb
@@ -0,0 +1,116 @@
+module PrettyText# :nodoc:
+  # This class represents a style used by the <tt>PrettyText</tt> class to generate
+  # an image. This style includes font properties like the font-file and the font-size,
+  # colors and text transformation like upcase.
+  #
+  # Possible style attributes are:
+  # [<tt>format</tt>]
+  #   Can either be <tt>:png</tt> or <tt>:gif</tt>, defaults to <tt>:png</tt>.
+  #   If <tt>:gif</tt> is used set the <tt>bg_color</tt> attribute to get proper
+  #   antialiasing.
+  # [<tt>color</tt>]
+  #   The color of the rendered text. Use HTML/CSS style color definitions (eg.:
+  #   ##fff, ##d2d2d2). Defaults to ##fff.
+  # [<tt>bg_color</tt>]
+  #   If you choose <tt>:gif</tt> as format the text will be rendered against the given
+  #   <tt>bg_color</tt> and the areas of the background which are not effected by the
+  #   antialiasing are cut out. This way you get proper antialiasing, although the gif
+  #   format does not support alpha channels.
+  # [<tt>size</tt>]
+  #   The size of the rendered font in points
+  # [<tt>font</tt>]
+  #   The font file to use relative to the font root directory, which is fonts by default
+  # [<tt>upcase</tt>]
+  #   This flag defines if the text is transformed to uppercase before rendering.
+  #   This boolean defaults to false.
+  # [<tt>extra_width</tt>]
+  #   Sometimes Imagemagick will calculate the wrong width for the image. You can add
+  #   some extra space by setting this value. Defaults to 0. This might be obsolete due
+  #   to an former error in the implementation.
+  #
+  # You can set the atrributes via the constructor:
+  #   style = Cabird::PrettyTextStyle.new({:format => :png, :color => '#f00'})
+  # or via the setter methods:
+  #   style.color = '#000'
+  class Style
+    #require 'RMagick'
+
+#    require 'digest/md5'
+    require 'unicode'
+    
+    @@default = {:format => :png,
+                 :color => '#000',
+                 :additional_colors => [],
+                 :bg_color => '#fff',
+                 :size => 12,
+                 :font => nil,
+                 :upcase => false,
+                 :extra_width => 0,
+                 :xextra => 0,
+                 :xoffset => 0,
+                 :yextra => 0,
+                 :yoffset => 0,
+                 :kerning => 0,
+                 :interline_spacing => 0,
+                 :upscale => false}
+
+    # define getters and setters with default
+    @@default.keys.each do |key|
+      # getter method
+      define_method key do
+        return @attributes[key] || @@default[key]
+      end
+
+      # setter method
+      define_method key.to_s + '=' do |value|
+        @attributes[key] = value
+      end
+    end
+
+    # Sets up a new <tt>PrettyTextStyle</tt> object. All of the above mentioned attributes
+    # can be set here.
+    def initialize(attributes = nil)
+      @attributes = attributes || Hash.new
+    end
+
+    # This method overrides the default getter method for the <tt>bg_color</tt> method.
+    def bg_color
+      return "transparent" if self.format == :png
+      return @attributes[:bg_color] || @@default[:bg_color]
+    end
+
+    # Applies all text transformations to a given text, which includes only the
+    # <tt>upcase</tt> option at the moment. Returns the transformed text.
+    def process_text(text)
+      return (self.upcase == true) ? Unicode::upcase(text) : text
+    end
+
+    # Generates a hash string of all attributes and a given string. This method is used
+    # by the <tt>generate_filename</tt> method to retrieve the filename for a combilnation
+    # of style attributes and a certain text.
+    def generate_hash(str = "")
+       to_hash = str
+       @@default.keys.each do |key|
+         to_hash += self.send(key).to_s
+       end
+       return  Digest::MD5.hexdigest(to_hash)
+    end
+
+    # This method generates a hashed filename of the style attributes and a given text
+    def generate_filename(str)
+      fragments = self.generate_hash(str).scan(/(..)/).flatten
+      return File.join(fragments) + "." + self.format.to_s
+    end
+
+    # Clones this <tt>PrettyTextStyle</tt> object and returns an altered version of the clone.
+    # You can override attributes with the argument.
+    def alter(attributes = Hash.new)
+      result = Hash.new
+      @@default.keys.each do |key|
+        result[key] = (attributes[key]) ? attributes[key] :
+                                          @attributes[key]
+      end
+      return self.class.new(result)
+    end
+  end
+end
